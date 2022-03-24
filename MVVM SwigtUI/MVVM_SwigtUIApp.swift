50eb9d1dc09fc912272c449e006d21130c203042
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-03-24 13:36:41 +0300

    Initial Commit

---
+++ MVVM SwigtUI/MVVM_SwigtUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwigtUIApp.swift
+//  MVVM SwigtUI
+//
+//  Created by Олейник Богдан on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwigtUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
